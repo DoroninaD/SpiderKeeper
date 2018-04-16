7e3deae3e4597c5c85aecf121f087278a403eedf
DaryaDoronina <darya.doronina@saritasa.com>
2018-04-16 16:38:22 +0700

    Change db to postgres

--- SpiderKeeper/config.py
+++ SpiderKeeper/config.py
@@ -8,9 +8,6 @@ DEBUG = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath('.'), 'SpiderKeeper.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_CONNECT_OPTIONS = {}
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
@@ -43,3 +40,23 @@ BASIC_AUTH_FORCE = True
 
 FILES_STORAGE = 'https://s3.amazonaws.com/dds-testing-bucket/scrapy'
 FILES_FORMAT = 'csv'
+
+# DB settings
+
+DATABASE = {
+    'DB': 'postgresql',
+    'NAME': 'usummit_grabbers',
+    'USER': 'usummit_grabbers_user',
+    'PASSWORD': 'manager',
+    'HOST': 'postgres',
+    'PORT': '5432',
+    'CONN_MAX_AGE': 30,
+}
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath('.'), 'SpiderKeeper.db')
+SQLALCHEMY_DATABASE_URI = '{DB}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'\
+    .format_map(DATABASE)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
